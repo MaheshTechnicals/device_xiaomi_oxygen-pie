276104e14de1e59cff19e038e1732af76a1458e8
R15Hi <rk.8969009@gmail.com>
2020-07-04 10:06:22 +0000

    havoc

--- oxygen/havoc_oxygen.mk
+++ oxygen/havoc_oxygen.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/oxygen/device.mk)
 
 # Inherit some common aosp  stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := oxygen
-PRODUCT_NAME := superior_oxygen
+PRODUCT_NAME := havoc_oxygen
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Max 2
 PRODUCT_MANUFACTURER := Xiaomi
@@ -47,3 +47,4 @@ TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Mahesh Technicals"
+
--- oxygen/vendorsetup.sh
+++ oxygen/vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo superior_oxygen-userdebug
+add_lunch_combo havoc_oxygen-userdebug
