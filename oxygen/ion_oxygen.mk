212134f56a606b5df7d751702a9a6856c254bbd1
MaheshTechnicals <msvarma10001@gmail.com>
2020-08-19 09:39:56 +0300

    bringup for ion

--- oxygen/ion_oxygen.mk
+++ oxygen/ion_oxygen.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/oxygen/device.mk)
 
 # Inherit some common aosp  stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/ion/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := oxygen
-PRODUCT_NAME := superior_oxygen
+PRODUCT_NAME := ion_oxygen
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Max 2
 PRODUCT_MANUFACTURER := Xiaomi
--- oxygen/vendorsetup.sh
+++ oxygen/vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo superior_oxygen-userdebug
+add_lunch_combo ion_oxygen-userdebug
